a22cbc52328a8afa85fc683a3ad1e11a0b27288b
Alec Ostheimer <alecostheimer94@gmail.com>
2022-10-04 12:07:58 -0600

    udpated

---
+++ components/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading, Box } from '@chakra-ui/react';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Heading size="2xl">Alec Ostheimer</Heading>
+    </Box>
+  );
+};
+
+export default HomePage;
